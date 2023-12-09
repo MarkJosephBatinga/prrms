3b95bf1452151fe0fc4b4a27de9069307c847318
Mark Joseph Batinga <91181348+MarkJosephBatinga@users.noreply.github.com>
2023-12-09 13:21:45 +0800

    Added approval_log table

---
+++ app/Models/ApprovalLog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Student;
+
+class ApprovalLog extends Model
+{
+    use HasFactory;
+
+    protected $table = 'approval_log'; // Specify the table name if different from the model name
+
+    protected $fillable = ['student_id', 'status', 'notes']; // Specify the columns that can be mass-assigned
+
+    // Define relationships if needed
+    public function student()
+    {
+        return $this->belongsTo(Student::class, 'student_id');
+    }
+}
