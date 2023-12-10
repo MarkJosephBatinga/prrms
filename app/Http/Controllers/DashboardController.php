87326e820386b3a87ef362dff5ce66a1bfc67c5d
Mark Joseph Batinga <91181348+MarkJosephBatinga@users.noreply.github.com>
2023-12-10 09:47:45 +0800

    Added data to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 use App\Models\Student;
+use App\Models\Program;
+use App\Models\User;
+use App\Models\ApprovalLog;
+use App\Models\Course;
 use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
@@ -12,7 +16,14 @@ class DashboardController extends Controller
         $userType = Auth::user()->user_type;
 
         if ($userType === 'admin') {
-            return view('dashboard.admin');
+            $data['user_count'] = User::where('student_id', '!=', 0)->count();
+            $data['program_count'] = Program::count();
+            $data['course_count'] = Course::count();
+            $data['prereg_count'] = ApprovalLog::where('status', 1)->count();
+            $data['endorsed_count'] = ApprovalLog::where('status', 2)->count();
+            $data['approved_count'] = ApprovalLog::where('status', 3)->count();
+
+            return view('dashboard.admin', $data);
         } else {
             $student_id = Auth::user()->student_id;
             $data['student'] = Student::with('program_info', 'course.course', 'approval_log')->find($student_id);
--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -12,46 +12,46 @@
     <div class="dashboard-cards">
         <div class="card-container">
             <i class='bx bx-file card-icon'></i>
-            <p class="card-text-heading">Pre-registered <br> Students</p>
-            <p class="card-text-count">23</p>
-            <a href=""><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
+            <p class="card-text-heading">Users</p>
+            <p class="card-text-count">{{$user_count}}</p>
+            <a href="{{route('dashboard')}}"><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
         </div>
 
         <div class="card-container">
             <i class='bx bx-folder-open card-icon'></i>
             <p class="card-text-heading">Programs</p>
-            <p class="card-text-count">10</p>
-            <a href=""><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
+            <p class="card-text-count">{{$program_count}}</p>
+            <a href="{{route('programs')}}"><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
         </div>
 
         <div class="card-container">
             <i class='bx bxs-user-account card-icon'></i>
-            <p class="card-text-heading">Users</p>
-            <p class="card-text-count">13</p>
-            <a href=""><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
+            <p class="card-text-heading">Courses</p>
+            <p class="card-text-count">{{$course_count}}</p>
+            <a href="{{route('courses')}}"><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
         </div>
     </div>
 
     <div class="dashboard-cards">
         <div class="card-container">
             <i class='bx bxs-user-account card-icon'></i>
-            <p class="card-text-heading">Students endorsed</p>
-            <p class="card-text-count">2</p>
-            <a href=""><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
+            <p class="card-text-heading">Pre-registered <br>Students</p>
+            <p class="card-text-count">{{$prereg_count}}</p>
+            <a href="{{route('records')}}"><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
         </div>
 
         <div class="card-container">
             <i class='bx bxs-user-account card-icon'></i>
-            <p class="card-text-heading">Students to be <br> endorsed</p>
-            <p class="card-text-count">2</p>
-            <a href=""><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
+            <p class="card-text-heading">Endorsed <br> Students</p>
+            <p class="card-text-count">{{$endorsed_count}}</p>
+            <a href="{{route('records')}}"><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
         </div>
 
         <div class="card-container">
             <i class='bx bxs-user-account card-icon'></i>
-            <p class="card-text-heading">Students to be <br> approved</p>
-            <p class="card-text-count">4</p>
-            <a href=""><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
+            <p class="card-text-heading">Approved <br> Students</p>
+            <p class="card-text-count">{{$approved_count}}</p>
+            <a href="{{route('records')}}"><i class='bx bx-arrow-back bx-rotate-180 card-nav-icon'></i></a>
         </div>
     </div>
 
